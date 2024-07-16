eddf0bfd41368e1d0b57dc9e4f505a1c2f9d0a4d
Michael Muniu <Macon4001:gmail.com>
2024-07-16 15:39:54 +0100

    Adding in the code to compute the dot product of the input_vector and weights_1 : This is what is used to figure out how similar the two lines are

--- Neural Network- Nas 100.py
+++ Neural Network- Nas 100.py
@@ -5,3 +5,9 @@ input_vector = [1.72, 1.23]
 weights_1 = [1.26, 0]
 weights_2 = [2.1, 0.32]
 
+# computing the dot product of input_vector and weights_1
+first_index_mult = input_vector[0] * weights_1[0]
+second_index_mult  = input_vector[1] * weights_1[1]
+dot_product_1 = first_index_mult * second_index_mult
+
+print(f"the dor product is:  {dot_product_1}")

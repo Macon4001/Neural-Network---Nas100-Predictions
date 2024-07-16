b0d0d5dbceeef8599c6412b8112303e1d7a8bb36
Michael Muniu <Macon4001:gmail.com>
2024-07-16 15:45:18 +0100

    Changes made to variable names aswell as spelling errors fixes

--- Neural Network- Nas 100.py
+++ Neural Network- Nas 100.py
@@ -1,13 +1,12 @@
 ## Neural Network for predicting Nas 100 Prices
 
-
 input_vector = [1.72, 1.23]
 weights_1 = [1.26, 0]
-weights_2 = [2.1, 0.32]
+weights_2 = [2.17, 0.32]
 
-# computing the dot product of input_vector and weights_1
-first_index_mult = input_vector[0] * weights_1[0]
-second_index_mult  = input_vector[1] * weights_1[1]
-dot_product_1 = first_index_mult * second_index_mult
+# Computing the dot product of input_vector and weights_1
+first_indexes_mult = input_vector[0] * weights_1[0]
+second_indexes_mult = input_vector[1] * weights_1[1]
+dot_product_1 = first_indexes_mult + second_indexes_mult
 
-print(f"the dor product is:  {dot_product_1}")
+print(f"The dot product is: {dot_product_1}")
